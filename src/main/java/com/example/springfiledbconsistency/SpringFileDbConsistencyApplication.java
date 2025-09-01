2f1d1e86a6318227ad578603b0835b6e27f7b7a2
kwongio <rldh11111@naver.com>
2025-09-02 00:26:21 +0900

    Initial commit

---
+++ src/main/java/com/example/springfiledbconsistency/SpringFileDbConsistencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfiledbconsistency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileDbConsistencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileDbConsistencyApplication.class, args);
+    }
+
+}
